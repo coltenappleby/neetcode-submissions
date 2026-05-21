f8d0b3aa29c0e4e7575ba9e44ded67367dc34f06
Colten Appleby <75281761+coltenappleby@users.noreply.github.com>
2026-05-21 12:07:40 -0700

    Add: contains-duplicate-ii - submission-11

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-11.py
@@ -0,0 +1,16 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        # Fixed Sliding Window with set
+        window = set()
+        L = 0
+        
+        for R in range(len(nums)):
+            # when R gets outside of the window we need to pull L to the right and fix the window
+            if R - L > k: # Window size check
+                window.remove(nums[L]) # what if the window contains multiple elements of the same number?
+                L += 1
+            if nums[R] in window:
+                return True
+            window.add(nums[R])
+
+        return False
